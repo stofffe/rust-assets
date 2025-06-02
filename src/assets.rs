67fdb006b1bb531c897190fbb21023d9976382d2
Christoffer Andersson <chan8@kth.se>
2025-06-02 23:09:41 +0200

    added async loading/reloading using threads

--- src/assets.rs
+++ src/assets.rs
@@ -1,6 +1,6 @@
 use crate::handle::AssetHandle;
 use std::{
-    any::Any,
+    any::{Any, TypeId},
     collections::{HashMap, HashSet},
     fs,
     marker::PhantomData,
@@ -11,26 +11,16 @@ use std::{
 
 pub type DynAsset = Box<dyn Asset>;
 
-pub trait Asset: Any {
-    fn deserialize(&mut self, _path: &Path) {
+pub trait Asset: Any + Send + Sync {
+    fn load(path: &Path) -> Self
+    where
+        Self: Sized;
+
+    fn reload(&mut self, _path: &Path) {
         println!("[WARN]: trying to deserialize asset without implementing trait")
     }
-    fn serialize(&mut self, _path: &Path) {
+    fn write(&mut self, _path: &Path) {
         println!("[WARN]: trying to serialize asset without implementing trait")
-    }
-}
-pub trait ReloadableAsset: Asset {
-    // replace with
-    fn load(path: &Path) -> Self;
-    fn write(&self, path: &Path);
-}
-
-impl<T: ReloadableAsset> Asset for T {
-    fn deserialize(&mut self, path: &Path) {
-        *self = T::load(path);
-    }
-    fn serialize(&mut self, path: &Path) {
-        self.write(path);
     }
 }
 
@@ -58,24 +48,30 @@ impl dyn RenderAsset {
         self
     }
 }
-
+type AssetLoaderFn = fn(&Path) -> Box<dyn Asset>;
 pub struct Assets {
-    cache: HashMap<AssetHandle<DynAsset>, DynAsset>,
+    cache: HashMap<AssetHandle<DynAsset>, Option<DynAsset>>,
     render_cache: HashMap<AssetHandle<DynAsset>, Box<dyn RenderAsset>>,
 
-    dirty: HashSet<AssetHandle<DynAsset>>,
-
     serialize_handles: HashMap<AssetHandle<DynAsset>, PathBuf>,
-    deserialize_handles: HashMap<PathBuf, AssetHandle<DynAsset>>,
+    serialize_dirty: HashSet<AssetHandle<DynAsset>>,
+
+    deserialize_handles: HashMap<PathBuf, (AssetHandle<DynAsset>, AssetLoaderFn)>,
+
+    // reloading
     reload_watcher: notify_debouncer_mini::Debouncer<notify_debouncer_mini::notify::FsEventWatcher>,
     reload_receiver: mpsc::Receiver<PathBuf>,
-    #[allow(dead_code)]
     reload_sender: mpsc::Sender<PathBuf>,
+
+    // async loading
+    loaded_sender: mpsc::Sender<(AssetHandle<DynAsset>, DynAsset)>,
+    loaded_receiver: mpsc::Receiver<(AssetHandle<DynAsset>, DynAsset)>,
 }
 
 impl Assets {
     pub fn new() -> Self {
         let (reload_sender, reload_receiver) = mpsc::channel();
+        let (loaded_sender, loaded_receiver) = mpsc::channel();
         let sender_copy = reload_sender.clone();
 
         let reload_watcher = notify_debouncer_mini::new_debouncer(
@@ -89,7 +85,7 @@ impl Assets {
                             .expect("could not send");
                     }
                 }
-                Err(e) => println!("Error {:?}", e),
+                Err(err) => println!("debounced result error: {}", err),
             },
         )
         .expect("could not create watcher");
@@ -97,45 +93,61 @@ impl Assets {
         Self {
             cache: HashMap::new(),
             render_cache: HashMap::new(),
-            dirty: HashSet::new(),
+            serialize_dirty: HashSet::new(),
             deserialize_handles: HashMap::new(),
             serialize_handles: HashMap::new(),
+
             reload_receiver,
             reload_sender,
             reload_watcher,
+
+            loaded_sender,
+            loaded_receiver,
         }
     }
+
+    //
+    // Assets
+    //
 
     pub fn insert<T: Asset + 'static>(&mut self, data: T) -> AssetHandle<T> {
         let handle = AssetHandle::<T>::new();
         self.cache
-            .insert(handle.clone().to_handle(), Box::new(data));
+            .insert(handle.clone().to_handle(), Some(Box::new(data)));
         handle
     }
 
-    pub fn get<T: Asset + 'static>(&mut self, handle: AssetHandle<T>) -> &T {
+    // TODO: add get_or_default (e.g. 1x1 white pixel for image)
+    //
+    // could return error union [Ok, Invalid, Loading]
+    pub fn get<T: Asset + 'static>(&mut self, handle: AssetHandle<T>) -> Option<&T> {
         self.cache
             .get(&handle.to_handle())
-            .and_then(|a| a.as_any().downcast_ref::<T>())
-            .unwrap()
+            .expect("invalid handle")
+            .as_ref()
+            .and_then(|asset| asset.as_any().downcast_ref::<T>())
     }
 
-    pub fn get_mut<T: Asset + 'static>(&mut self, handle: AssetHandle<T>) -> &mut T {
+    pub fn get_mut<T: Asset + 'static>(&mut self, handle: AssetHandle<T>) -> Option<&mut T> {
         // invalidate gpu cache
         self.render_cache.remove(&handle.clone().to_handle());
 
         // set dirty
-        self.dirty.insert(handle.clone().to_handle());
-        println!("set dirty {:?}", handle.id);
+        self.serialize_dirty.insert(handle.clone().to_handle());
 
         // get value and convert to T
         self.cache
             .get_mut(&handle.to_handle())
-            .and_then(|a| a.as_any_mut().downcast_mut::<T>())
-            .unwrap()
+            .expect("invalid handle")
+            .as_mut()
+            .and_then(|asset| asset.as_any_mut().downcast_mut::<T>())
     }
 
-    pub fn load_from_disk<T: ReloadableAsset + 'static>(
+    //
+    // Reloading
+    //
+
+    pub fn load_sync<T: Asset + 'static>(
         &mut self,
         path: &Path,
         watch: bool,
@@ -146,7 +158,7 @@ impl Assets {
         let data = T::load(&path);
         let handle = AssetHandle::<T>::new();
         self.cache
-            .insert(handle.clone().to_handle(), Box::new(data));
+            .insert(handle.clone().to_handle(), Some(Box::new(data)));
 
         if watch {
             self.reload_watcher
@@ -157,8 +169,10 @@ impl Assets {
                 )
                 .unwrap();
 
-            self.deserialize_handles
-                .insert(path.clone(), handle.clone().to_handle());
+            self.deserialize_handles.insert(
+                path.clone(),
+                (handle.clone().to_handle(), |p| Box::new(T::load(p))),
+            );
         }
 
         if write {
@@ -169,28 +183,114 @@ impl Assets {
         handle
     }
 
-    // pub fn watch<T: ReloadableAsset + 'static>(&mut self, path: &Path) -> AssetHandle<T> {
-    //     let path = fs::canonicalize(path).unwrap();
+    pub fn load_async<T: Asset + 'static>(
+        &mut self,
+        path: &Path,
+        watch: bool,
+        write: bool,
+    ) -> AssetHandle<T> {
+        let path = fs::canonicalize(path).unwrap();
+
+        let handle = AssetHandle::<T>::new();
+        self.cache.insert(handle.clone().to_handle(), None);
+
+        let path_clone = path.clone();
+        let handle_clone = handle.clone();
+        let loaded_sender_clone = self.loaded_sender.clone();
+
+        std::thread::spawn(move || {
+            println!("start async load");
+            std::thread::sleep(Duration::from_millis(2000));
+            let data = T::load(&path_clone);
+            loaded_sender_clone
+                .send((handle_clone.to_handle(), Box::new(data)))
+                .expect("could not send");
+            println!("end async load");
+        });
+
+        if watch {
+            self.reload_watcher
+                .watcher()
+                .watch(
+                    &path,
+                    notify_debouncer_mini::notify::RecursiveMode::Recursive,
+                )
+                .unwrap();
+
+            self.deserialize_handles.insert(
+                path.clone(),
+                (handle.clone().to_handle(), |p| Box::new(T::load(p))),
+            );
+        }
+
+        if write {
+            self.serialize_handles
+                .insert(handle.clone().to_handle(), path.clone());
+        }
+
+        handle
+    }
+
+    pub fn poll_write(&mut self) {
+        for handle in self.serialize_dirty.drain() {
+            if let Some(path) = self.serialize_handles.get(&handle) {
+                let asset = self.cache.get_mut(&handle).expect("invalid handle");
+                if let Some(asset) = asset {
+                    asset.write(path);
+                    println!("write {:?}", path);
+                }
+            }
+        }
+    }
+
+    // checks if any files changed and spawns a thread which reloads the data
+    pub fn poll_reload(&mut self) {
+        for path in self.reload_receiver.try_iter() {
+            let (handle, factory) = self.deserialize_handles.get_mut(&path).unwrap();
+
+            //
+            // SYNC
+            //
+            let asset = self.cache.get_mut(handle).expect("invalid handle");
+            if let Some(asset) = asset {
+                asset.reload(&path);
+                println!("load {:?}", path);
+            }
+
+            //
+            // ASYNC
+            //
+
+            let handle_clone = handle.clone();
+            let path_clone = path.clone();
+            let sender_clone = self.loaded_sender.clone();
+            let factory_clone = factory.clone();
+            std::thread::spawn(move || {
+                // println!("reload start {:?}", path);
+                std::thread::sleep(Duration::from_millis(10000));
+                sender_clone
+                    .send((handle_clone, factory_clone(&path_clone)))
+                    .expect("could not send");
+                // println!("reload end {:?}", path);
+            });
+        }
+    }
+
+    // check if any files completed loading and update cache and invalidate render cache
+    pub fn poll_loaded(&mut self) {
+        for (handle, asset) in self.loaded_receiver.try_iter() {
+            self.cache.insert(handle.clone(), Some(asset));
+            self.render_cache.remove(&handle);
+        }
+    }
+
+    pub fn force_reload(&self, path: PathBuf) {
+        self.reload_sender.send(path).expect("could not send path");
+    }
+
     //
-    //     let handle = self.load_from_disk(&path, false, false);
+    // Render assets
     //
-    //     self.reload_watcher
-    //         .watcher()
-    //         .watch(
-    //             &path,
-    //             notify_debouncer_mini::notify::RecursiveMode::Recursive,
-    //         )
-    //         .unwrap();
-    //
-    //     self.deserialize_handles
-    //         .insert(path.clone(), handle.clone().to_handle());
-    //     self.serialize_handles
-    //         .insert(handle.clone().to_handle(), path.clone());
-    //
-    //     println!("set H2P {:?} to {:?}", handle.id, path);
-    //
-    //     handle
-    // }
 
     pub fn convert<G: ConvertableRenderAsset>(
         &mut self,
@@ -199,8 +299,8 @@ impl Assets {
     ) -> &G {
         // create new if not in cache
         if !self.render_cache.contains_key(&handle.clone().to_handle()) {
-            let cpu = self.get(handle.clone());
-            let converted = G::convert(cpu, params);
+            let asset = self.get(handle.clone()).expect("invalid handle"); // TODO: handle
+            let converted = G::convert(asset, params);
             self.render_cache
                 .insert(handle.clone().to_handle(), Box::new(converted));
         }
@@ -219,8 +319,8 @@ impl Assets {
     ) -> &mut G {
         // create new if not in cache
         if !self.render_cache.contains_key(&handle.clone().to_handle()) {
-            let cpu = self.get_mut(handle.clone());
-            let converted = G::convert(cpu, params);
+            let asset = self.get_mut(handle.clone()).expect("invalid handle"); // TODO: hanlde
+            let converted = G::convert(asset, params);
             self.render_cache
                 .insert(handle.clone().to_handle(), Box::new(converted));
         }
@@ -230,29 +330,6 @@ impl Assets {
             .get_mut(&handle.to_handle())
             .and_then(|a| a.as_any_mut().downcast_mut::<G>())
             .unwrap()
-    }
-
-    pub fn poll_serialize(&mut self) {
-        for handle in self.dirty.drain() {
-            if let Some(path) = self.serialize_handles.get(&handle) {
-                self.cache.get_mut(&handle).unwrap().serialize(path);
-            }
-        }
-    }
-    pub fn poll_deserialize(&mut self) {
-        for path in self.reload_receiver.try_iter() {
-            let handle = self.deserialize_handles.get_mut(&path).unwrap();
-
-            // reload
-            self.cache.get_mut(handle).unwrap().deserialize(&path);
-
-            // invalidate potential gpu cache
-            self.render_cache.remove(handle);
-        }
-    }
-
-    pub fn force_reload(&self, path: PathBuf) {
-        self.reload_sender.send(path).expect("could not send path");
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use assets::{Assets, ConvertableRenderAsset, ReloadableAsset, RenderAsset};
+use assets::{Asset, Assets, ConvertableRenderAsset, RenderAsset};
 use std::{fmt::Write, fs::read_to_string, path::Path, thread::sleep, time::Duration};
 
 mod assets;
@@ -11,26 +11,33 @@ fn main() {
         name: String::from("bro"),
         age: 12,
     });
-    let person2 = assets.load_from_disk::<Person>(Path::new("assets/alice.person"), true, true);
-    let person3 = assets.load_from_disk::<Person>(Path::new("assets/bob.person"), true, false);
-    let house1 = assets.load_from_disk::<House>(Path::new("assets/house1"), true, true);
-    let shader = assets.load_from_disk::<Shader>(Path::new("assets/shader"), true, false);
+    let person2 = assets.load_async::<Person>(Path::new("assets/alice.person"), true, true);
+    let shader = assets.load_sync::<Shader>(Path::new("assets/shader"), true, false);
 
-    assets.get_mut(person1.clone());
-
+    let mut i = 0;
     loop {
         sleep(Duration::from_millis(1000));
+        if i % 5 == 0 {
+            // update manually
+            if let Some(person) = assets.get_mut(person2.clone()) {
+                person.age += 1;
+            }
+        }
 
         println!("shader: {:?}", assets.get(shader.clone()));
         let gpu_shader = assets.convert(shader.clone(), &100);
         print_gpu_shader(gpu_shader);
 
-        println!("person2: {:?}", assets.get(person2.clone()));
-        assets.get_mut(person2.clone()).age += 1;
-        assets.get_mut(house1.clone()).price += 1;
+        if let Some(person) = assets.get_mut(person2.clone()) {
+            println!("person {:?}", person);
+        } else {
+            println!("person not loaded");
+        }
 
-        assets.poll_serialize();
-        assets.poll_deserialize();
+        assets.poll_reload();
+        assets.poll_loaded();
+
+        i += 1;
     }
 }
 
@@ -39,18 +46,50 @@ fn print_gpu_shader(shader: &GpuShader) {
 }
 
 #[derive(Debug)]
+struct Person {
+    name: String,
+    age: u32,
+}
+
+impl Asset for Person {
+    fn load(path: &Path) -> Self {
+        let inp = read_to_string(path).unwrap();
+        let mut split = inp.split_whitespace();
+        let name = split.next().unwrap().to_string();
+        let age = split.next().unwrap().parse::<u32>().unwrap();
+        Self { name, age }
+    }
+
+    fn reload(&mut self, path: &Path) {
+        *self = Self::load(path);
+    }
+
+    fn write(&mut self, path: &Path) {
+        let mut output = String::new();
+        output.write_str(&self.name).unwrap();
+        output.write_char(' ').unwrap();
+        output.write_str(&self.age.to_string()).unwrap();
+        std::fs::write(path, output).expect("could not write to person");
+    }
+}
+
+#[derive(Debug)]
 struct Shader {
     source: String,
 }
 
-impl ReloadableAsset for Shader {
+impl Asset for Shader {
     fn load(path: &Path) -> Self {
         println!("reload shader");
         let content = read_to_string(path).expect("could not read shader from disk");
         Self { source: content }
     }
 
-    fn write(&self, path: &Path) {
+    fn reload(&mut self, path: &Path) {
+        *self = Self::load(path);
+    }
+
+    fn write(&mut self, path: &Path) {
         std::fs::write(path, &self.source).expect("could not write shader to disk");
     }
 }
@@ -71,46 +110,5 @@ impl ConvertableRenderAsset for GpuShader {
         Self {
             module: id + *params,
         }
-    }
-}
-
-#[derive(Debug)]
-struct House {
-    price: u32,
-}
-
-impl ReloadableAsset for House {
-    fn load(path: &Path) -> Self {
-        let inp = read_to_string(path).unwrap();
-        let mut split = inp.split_whitespace();
-        let price = split.next().unwrap();
-        println!("{price}");
-        let price = price.parse::<u32>().unwrap();
-        Self { price }
-    }
-
-    fn write(&self, path: &Path) {}
-}
-
-#[derive(Debug)]
-struct Person {
-    name: String,
-    age: u32,
-}
-
-impl ReloadableAsset for Person {
-    fn load(path: &Path) -> Self {
-        let inp = read_to_string(path).unwrap();
-        let mut split = inp.split_whitespace();
-        let name = split.next().unwrap().to_string();
-        let age = split.next().unwrap().parse::<u32>().unwrap();
-        Self { name, age }
-    }
-    fn write(&self, path: &Path) {
-        let mut output = String::new();
-        output.write_str(&self.name).unwrap();
-        output.write_char(' ').unwrap();
-        output.write_str(&self.age.to_string()).unwrap();
-        std::fs::write(path, output).expect("could not write to person");
     }
 }
